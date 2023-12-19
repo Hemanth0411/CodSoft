a742dc5472450b5fc404f7bba459cf3683887248
Hemanth0411 <151835955+Hemanth0411@users.noreply.github.com>
2023-12-19 17:21:10 +0530

    Update ToDoApp.py
    Added a function to delete the selected task in the list box whenever the remove task button is pressed. Made the code look a bit organized.

--- ToDoApp.py
+++ ToDoApp.py
@@ -2,6 +2,7 @@
 #TO-DO LIST 
 from tkinter import *
 from tkinter import messagebox
+
 def adding_task():
     task=entry.get()
     if task:
@@ -9,17 +10,30 @@ def adding_task():
         entry.delete(0, END)
     else:
         messagebox.showwarning("Warning!", "Please enter a task!")
+
+def removing_task():
+    try:
+        selected=list_box.curselection()[0]
+        list_box.delete(selected)
+    except:
+        messagebox.showwarning("Warning", "Please select a task to remove!")
+    
     
 root=Tk()
 root.title("To Do List")
+
 entry=Entry(root, font=("Arial",20))
 entry.grid(row=0, column=0,padx=10, pady=10,sticky="we")
+
 add_task=Button(root, text="Add Task", font=("Arial",16), command=adding_task)
-add_task.grid(row=0, column=1,pady=10, sticky="we")
-list_box=Listbox(root, font=("Arial",20))
+add_task.grid(row=0, column=1,pady=10,padx=10, sticky="we")
+
+list_box=Listbox(root, font=("Arial",20), selectmode=SINGLE)
 list_box.grid(row=1, column=0, columnspan=2,padx=10, pady=10, sticky="nswe")
-remove_task= Button(root, text="Remove Task", font=("Arial",16))
+
+remove_task= Button(root, text="Remove Task", font=("Arial",16),command=removing_task)
 remove_task.grid(row=2,column=0,columnspan=2, padx=10, pady=10, sticky="we")
+
 root.grid_rowconfigure(1, weight=1)
 root.grid_columnconfigure(0, weight=1)
 
