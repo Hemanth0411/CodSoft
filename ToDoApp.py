7a6be3f7243af3038a1bc9e48b5310a8988d5da1
Hemanth0411 <151835955+Hemanth0411@users.noreply.github.com>
2023-12-25 17:47:14 +0530

    Update ToDoApp.py
    Made the whole code look more elegant by giving appropriate spaces. Added colors to the app to make the app look more pleasing.

--- ToDoApp.py
+++ ToDoApp.py
@@ -21,18 +21,19 @@ def removing_task():
     
 root=Tk()
 root.title("To Do List")
+root.config(bg="#333333")
 
-entry=Entry(root, font=("Arial",20))
-entry.grid(row=0, column=0,padx=10, pady=10,sticky="we")
+entry=Entry(root, font=("Arial", 20), bg="#e5e5e5", fg="#000000")
+entry.grid(row=0, column=0, padx=10, pady=10, sticky="we")
 
-add_task=Button(root, text="Add Task", font=("Arial",16), command=adding_task)
-add_task.grid(row=0, column=1,pady=10,padx=10, sticky="we")
+add_task=Button(root, text="Add Task", font=("Arial", 16), bg="#e7a236", fg="#000000", command=adding_task)
+add_task.grid(row=0, column=1, pady=10, padx=10, sticky="we")
 
-list_box=Listbox(root, font=("Arial",20), selectmode=SINGLE)
-list_box.grid(row=1, column=0, columnspan=2,padx=10, pady=10, sticky="nswe")
+list_box=Listbox(root, font=("Arial", 20), selectmode=SINGLE, bg="#ffffff")
+list_box.grid(row=1, column=0, columnspan=2, padx=10, pady=10, sticky="nswe")
 
-remove_task= Button(root, text="Remove Task", font=("Arial",16),command=removing_task)
-remove_task.grid(row=2,column=0,columnspan=2, padx=10, pady=10, sticky="we")
+remove_task= Button(root, text="Remove Task", font=("Arial", 16), bg="#e7a236", fg="#000000", command=removing_task)
+remove_task.grid(row=2, column=0, columnspan=2, padx=10, pady=10, sticky="we")
 
 root.grid_rowconfigure(1, weight=1)
 root.grid_columnconfigure(0, weight=1)
